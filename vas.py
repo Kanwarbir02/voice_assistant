712aff943dc58bc537c6e91dd46c4fa06291d483
Kanwarbir02 <kanwarbir2002@gmail.com>
2021-08-18 14:19:59 +0530

    Add greet Functionality

--- vas.py
+++ vas.py
@@ -2,7 +2,22 @@ import pyttsx3
 import datetime
 
 engine = pyttsx3.init()
-engine.say("Hello I am Your Voice assistant")
-engine.runAndWait()
 
-print(datetime.datetime.now())
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def greet():
+    currentHour = int(datetime.datetime.now().hour)
+    if currentHour>0 and currentHour<12:
+        speak("Good Morning Sir!")
+    elif currentHour>12 and currentHour<16:
+        speak("Good Afternoon Sir!")    
+    else:
+        speak("Good Evening Sir!")
+
+    speak("How may I help you?")
+
+if __name__ == "__main__":
+    greet()

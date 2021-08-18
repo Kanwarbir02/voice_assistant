1a3caeb3d00eb3041eb2f62e1d1520468642e6d3
Kanwarbir02 <kanwarbir2002@gmail.com>
2021-08-18 17:14:51 +0530

    Add voice inputs

--- vas.py
+++ vas.py
@@ -1,4 +1,5 @@
 import pyttsx3 
+import speech_recognition as sr
 import datetime
 
 engine = pyttsx3.init()
@@ -19,5 +20,24 @@ def greet():
 
     speak("How may I help you?")
 
+def voiceInput():
+    r= sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        # r.pause_threshold=1
+        # r.energy_threshold=300
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio)
+        print(f"User said: {query}")
+
+    except Exception as e:
+        print(e)
+        speak("Please try again, I could not listen properly.")     
+        return "None" 
+    return query
+
 if __name__ == "__main__":
-    greet()
+    voiceInput()
